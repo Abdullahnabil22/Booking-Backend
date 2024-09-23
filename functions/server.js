858726a7af685ae1833640f772dcafda5a16fd35
Abdullahnabil22 <abdullahn.work@gmail.com>
2024-09-23 23:24:23 +0300

    netlify

--- functions/server.js
+++ functions/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const serverless = require("serverless-http");
-const app = express();
 require("dotenv").config();
 
 const messageRoutes = require("../routes/message");
@@ -15,33 +14,52 @@ const hostRouter = require("../routes/hosts");
 const userRouter = require("../routes/hostUser");
 const usersRouter = require("../routes/user");
 
+const app = express();
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+// Root route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API is working" });
+});
+
 // Use your routes
-app.use("/.netlify/functions/server/host", hostRouter);
-app.use("/.netlify/functions/server/hostUser", userRouter);
-app.use("/.netlify/functions/server/messages", messageRoutes);
-app.use("/.netlify/functions/server/reviews", reviewRoutes);
-app.use("/.netlify/functions/server/flights", flightRoutes);
-app.use("/.netlify/functions/server/roomTypes", roomTypeRoute);
-app.use("/.netlify/functions/server/bookings", bookingRoute);
-app.use("/.netlify/functions/server/carRentals", carRentalRoute);
-app.use("/.netlify/functions/server/user", usersRouter);
+app.use("/host", hostRouter);
+app.use("/hostUser", userRouter);
+app.use("/messages", messageRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/flights", flightRoutes);
+app.use("/roomTypes", roomTypeRoute);
+app.use("/bookings", bookingRoute);
+app.use("/carRentals", carRentalRoute);
+app.use("/user", usersRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json("Wrong Path");
 });
 
-const connect = async () => {
+// Database connection function (to be used in route handlers)
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("connected to booking db");
+    console.log("Connected to booking db");
   } catch (error) {
-    console.log(error.message);
+    console.error("Database connection error:", error);
+    throw error;
   }
 };
 
-connect();
+// Example of how to use connectDB in a route
+app.get("/test-db", async (req, res) => {
+  try {
+    await connectDB();
+    res.status(200).json({ message: "Database connection successful" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Database connection failed", error: error.message });
+  }
+});
 
 module.exports.handler = serverless(app);
