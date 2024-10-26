220aef875e0855fe1197894652159b640fdd45d2
rabab alaa <161600491+Rabab17@users.noreply.github.com>
2024-10-26 05:33:28 -0700

    update user

--- models/user.js
+++ models/user.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
 const bcryptjs = require("bcryptjs");
+const crypto = require("crypto"); 
 
 let users = mongoose.Schema(
   {
     userName: {
       type: String,
-      required: [true, "userName is required"],
-      unique: true,
+      // required: [true, "userName is required"],
+ unique: false,
+   defualt:"user"
     },
     firstName: {
       type: String,
-      required: [true, "firstName is required"],
+      // required: [true, "firstName is required"],
     },
     lastName: {
       type: String,
-      required: [true, "lastName is required"],
+      // required: [true, "lastName is required"],
     },
     email: {
       type: String,
@@ -23,28 +25,35 @@ let users = mongoose.Schema(
     },
     nationality: {
       type: String,
-      required: [true, "nationality is required"],
+      // required: [true, "nationality is required"],
     },
     numberOfMembers: {
       type: Number,
-      required: [true, "numberOfMembers are required"],
+      // required: [true, "numberOfMembers are required"],
     },
     phoneNumber: {
       type: String,
-      required: [true, "phoneNumber is required"],
+      // required: [true, "phoneNumber is required"],
     },
     password: {
       type: String,
       required: [true, "password is required"],
-      match: [
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-        "Password must be at least 8 characters long and contain at least one letter and one number.",
-      ],
+      // match: [
+      //   /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+      //   "Password must be at least 8 characters long and contain at least one letter and one number.",
+      // ],
     },
+    passwordResetToken:String,
+ 
+    passwordResetExpires:Date,
+    
+
+
     role: {
       type: String,
-      required: [true, "role is required"],
+      // required: [true, "role is required"],
       enum: ["admin", "user", "owner"],
+      defualt:"user"
     },
     active: {
       type: Boolean,
@@ -52,7 +61,17 @@ let users = mongoose.Schema(
   },
   { timestamps: true }
 );
-
+users.methods.createResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.passwordResetExpires = Date.now() + 15 * 60 * 1000;
+  console.log("hamada")
+  
+  console.log(resetToken, this.passwordResetToken);
+  
+  return resetToken;
+   }
 users.pre("save", async function (next) {
   if (this.isModified("password")) {
     const salt = await bcryptjs.genSalt(10);
